51be76770e542d23f0cee8d42e91c5ae3e4e5dc0
Shahar David <shahard333@gmail.com>
2021-03-11 14:16:54 +0200

    * Expose native toast show function to JS

--- android/src/main/java/com/reactlibrary/MyToastModule.java
+++ android/src/main/java/com/reactlibrary/MyToastModule.java
@@ -1,5 +1,8 @@
 package com.reactlibrary;
 
+import android.content.Context;
+import android.widget.Toast;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -20,8 +23,8 @@ public class MyToastModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void show(String text) {
+        Context context = getReactApplicationContext();
+        Toast.makeText(context,text, Toast.LENGTH_LONG).show();
     }
 }
--- ios/MyToast.m
+++ ios/MyToast.m
@@ -1,13 +1,34 @@
 #import "MyToast.h"
+#import "IOSNativeToast.h"
+@interface MyToast()
+@property (nonatomic, retain) IOSNativeToast *toast;
+@end
 
 @implementation MyToast
 
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    self.toast = [[IOSNativeToast alloc] init];
+  }
+  return self;
+}
+
++ (BOOL)requiresMainQueueSetup
+{
+  return YES;
+}
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(show:(NSString *)text)
 {
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+  [self.toast showToast:text];
 }
 
 @end
